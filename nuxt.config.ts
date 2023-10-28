d4500b2a19b91091d7e928b46c7c6d8a9b14a75e
Lejito <alejocrrs@gmail.com>
2023-10-27 22:54:12 -0500

    Calcular IC para media (parcial)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,5 +11,15 @@ export default defineNuxtConfig({
     vuetifyOptions: {
       /* vuetify options */
     }
+  },
+  app: {
+    head: {
+      title: 'SystemSE ~ Calculadora de intervalos de confianza',
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1'
+    }
+  },
+  build: {
+    transpile: ['js-stats']
   }
 })
